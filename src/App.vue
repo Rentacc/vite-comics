34197dac34ad4926a592a23f249ac2c7b6791fe2
rentacc <rtcommercialert@gmail.com>
2023-05-19 00:54:25 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,13 @@ import AppHeader from './components/AppHeader.vue';
  //const AppHeader = valore esportato dal file importato
  import AppFooter from './components/AppFooter.vue';
  import AppContent from './components/AppContent.vue';
- import AppContent2 from './components/AppContent2.vue';
- import AppContent3 from './components/AppContent3.vue';
+ 
 export default{
   components: {
     AppHeader,
     AppFooter,
     AppContent,
-    AppContent2,
-    AppContent3,
+   
   },
 
 };
@@ -21,8 +19,6 @@ export default{
 <template>
 <app-header></app-header>
 <AppContent></AppContent>
-<AppContent2></AppContent2>
-<AppContent3></AppContent3>
 <AppFooter></AppFooter>
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,37 @@
 <script>
-
-
 </script>
 
 <template>
-
+    <div class="bar">
+        <img class="size" src="../assets/img/dc-logo.png" alt="">
+        <ul class="text">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
+ 
 </template>
 
-<style>
+<style lang="scss" scoped >
+@use './../assets/style/partials/_variables' as *;
+.bar{
+    @include nav-bar;
+}
 
+.text{
+    @include color-text;
+}
+
+.size{
+    @include size-img;
+}
 </style>
